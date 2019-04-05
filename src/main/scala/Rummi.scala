e83fc7eef08dcd45e6664d1c6e6b9e560683eec8
Julian Riegraf <julian.riegraf@doubleslash.de>
2019-04-06 01:04:26 +0200

    added some case for basic functionality

---
+++ src/main/scala/Rummi.scala
@@ -0,0 +1,12 @@
+import de.htwg.se.rummi.model.RummiColors._
+
+object Rummi {
+  def main(args: Array[String]): Unit = {
+    BLUE.printInColor("\nthis should be in blue")
+    RED.printInColor("\nthis should be in red")
+    YELLOW.printInColor("\nthis should be in yellow")
+    GREEN.printInColor("\nthis should be in green")
+  }
+}
+
+
