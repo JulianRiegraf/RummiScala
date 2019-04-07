b9fc9e9dec834015ed685fbef3a97ee1b4da91b0
Julian Riegraf <julian.riegraf@doubleslash.de>
2019-04-07 14:58:37 +0200

    a tile can now be a joker

--- src/main/scala/de/htwg/se/rummi/model/Tile.scala
+++ src/main/scala/de/htwg/se/rummi/model/Tile.scala
@@ -2,7 +2,10 @@ package de.htwg.se.rummi.model
 
 import de.htwg.se.rummi.model.RummiColors.{Color}
 
-case class Tile(number: Int, color: Color) extends Ordered[Tile] {
+case class Tile(number: Int, color: Color, joker: Boolean) extends Ordered[Tile] {
+
+  def this(number: Int, color: Color) = this(number, color, false)
+
 
   override def compare(that: Tile): Int = number.compareTo(that.number)
 }
