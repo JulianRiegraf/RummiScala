7d7b8a54c0be4f615d3e051205689c8e1b6e04fc
Julian Riegraf <julian.riegraf@doubleslash.de>
2019-06-12 15:21:07 +0200

    added some specs

--- src/test/scala/de/htwg/se/rummi/model/RummiSetSpec.scala
+++ src/test/scala/de/htwg/se/rummi/model/RummiSetSpec.scala
@@ -1,7 +1,10 @@
 package de.htwg.se.rummi.model
 
+
 import de.htwg.se.rummi.model.RummiColors._
 import org.scalatest.{Matchers, WordSpec}
+
+import scala.util.Random
 
 class RummiSetSpec extends WordSpec with Matchers {
 
@@ -11,26 +14,67 @@ class RummiSetSpec extends WordSpec with Matchers {
     val blueTile1 = new Tile(1, BLUE)
     val yellowTile1 = new Tile(1, YELLOW)
 
-    val group1 = new RummiSet(greenTile1 :: blueTile1 :: yellowTile1 :: Nil)
 
-      "return true" in {
-        group1.isValidGroup() should be(true)
-      }
+    "return true" in {
+      val group1 = new RummiSet(greenTile1 :: blueTile1 :: yellowTile1 :: Nil)
+      group1.isValidGroup() should be true
     }
 
+    "give the correct amount of points" in {
+      val group1 = new RummiSet(greenTile1 :: blueTile1 :: yellowTile1 :: Nil)
+      group1.getPoints() should be 3
+    }
+  }
+
   "A run is composed of three or more, same-colored tiles, in consecutive number order. A Run" when {
+
+    val g1 = new Tile(1, GREEN)
+    val g2 = new Tile(2, GREEN)
+    val g3 = new Tile(3, GREEN)
+    val b3 = new Tile(3, BLUE)
+
     "to be constructed" should {
       "be return true if valid" in {
-        val greenTile1 = new Tile(1, GREEN)
-        val greenTile2 = new Tile(2, GREEN)
-        val greenTile3 = new Tile(3, GREEN)
-        val blueTile3 = new Tile(3, BLUE)
 
-        val run1 = new RummiSet(greenTile1 :: greenTile2 :: greenTile3 :: Nil)
-        run1.isValidRun() should be(true)
 
-        val run2 = new RummiSet(greenTile1 :: greenTile2 :: blueTile3 :: Nil)
-        run2.isValidRun() should be(false)
+        val run1 = new RummiSet(g1 :: g2 :: g3 :: Nil)
+        run1.isValidRun() should be true
+
+        val run2 = new RummiSet(g1 :: g2 :: b3 :: Nil)
+        run2.isValidRun() should be false
+      }
+
+      "be valid with shuffeld tiles" in {
+
+        var list = g1 :: g2 :: g3 :: Nil
+        list = Random.shuffle(list)
+
+        val run1 = new RummiSet(list)
+        run1.isValidRun() should be true
+
+      }
+
+      "be valid with a jocker at the end" in {
+        val joker: Tile = new Tile(-1, GREEN, true)
+        val list =  g1 :: g2 :: g3 :: joker :: Nil
+
+        val run = new RummiSet(list)
+        run.isValidRun() should be true
+      }
+
+      "be valid with a jocker at the beginning" in {
+        val joker: Tile = new Tile(-1, GREEN, true)
+        val list =  joker :: g1 :: g2 :: g3 :: Nil
+
+        val run = new RummiSet(list)
+        run.isValidRun() should be true
+      }
+
+      "be return the correct number of points" in {
+        var list = g1 :: g2 :: g3 :: Nil
+
+        val run1 = new RummiSet(list)
+        run1.getPoints() should be 6
       }
     }
   }
