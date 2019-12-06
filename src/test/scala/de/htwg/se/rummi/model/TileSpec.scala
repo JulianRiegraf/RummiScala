b246884518b1f98562940f4f6b6b748d703a8de6
Julian Riegraf <julianriegraf@web.de>
2019-12-06 12:08:48 +0100

    did some testing

--- src/test/scala/de/htwg/se/rummi/model/TileSpec.scala
+++ src/test/scala/de/htwg/se/rummi/model/TileSpec.scala
@@ -1,14 +1,35 @@
 package de.htwg.se.rummi.model
 
 import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json.Json
 
 class TileSpec extends WordSpec with Matchers {
 
   "A tile is a game piece that has a number and a color or is a jocker." should {
 
+    val tileBlueFour = Tile(4, BLUE, false)
 
-    "return true" in {
+    "convert to xml" in {
+      tileBlueFour.toXml should be
+      <tile>
+        <number>4</number>
+        <color>BLUE</color>
+        <joker>false</joker>
+      </tile>
+    }
 
+    "convert to json" in {
+      tileBlueFour.toJson should be
+        Json.parse("{\"number\":4,\"color\":\"BLUE\",\"joker\":false}")
+    }
+
+    "equals compares by reference, not by value" in {
+      tileBlueFour equals tileBlueFour should be(true)
+      tileBlueFour equals Tile(4, BLUE, false) should be(false)
+    }
+
+    "equals with another type should fail" in {
+      tileBlueFour equals "some random string" should be(false)
     }
 
   }
